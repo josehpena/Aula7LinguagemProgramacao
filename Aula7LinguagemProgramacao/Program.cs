9b08d5cad7363f012ae878cd44464ada6a502ab8
Jose Pena <josehpena2@gmail.com>
2022-09-21 21:09:55 -0300

    first commit

---
+++ Aula7LinguagemProgramacao/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula7LinguagemProgramacao
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            src.VerificaPares teste = new src.VerificaPares(10);
+
+            while (!teste.terminou())
+            {
+                if(teste.par())
+                    Console.WriteLine("{0} é par", teste.Valor);
+
+                teste.progride();
+            }
+
+            Console.WriteLine("Fim");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Aula7LinguagemProgramacao/src/VerificaPares.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula7LinguagemProgramacao.src
+{
+    internal class VerificaPares
+    {
+        private int valor;
+        List<int> list = new List<int>();
+        int[] vetor;
+
+        public int Valor
+        {
+            get { return valor; }
+            set { valor = value; }
+        }
+
+        public VerificaPares(int valor) { 
+
+            this.valor = valor;
+            
+        }
+
+        public bool terminou()
+        {
+            if(this.valor == 0)
+            {
+                vetor = new int[list.Count];
+                for(int i = 0; i < vetor.Length;i++)
+                {
+                    vetor[i] = list[i];
+                }
+                return true;
+            }
+
+            return false;
+
+        }
+
+        public void par()
+        {
+            if (this.valor % 2 == 0)
+            {
+                list.Add(this.valor);
+            }
+        }
+
+        public void progride()
+        {
+            this.valor--;
+        }
+    }
+}
